44221bf34547fbd3470b3c051aacf0ff9a900d5c
Dmitry Livchak <dmitry.livchak@gmail.com>
2018-03-23 13:25:51 +0000

    Fetch animals from source API

--- handler.js
+++ handler.js
@@ -1,10 +1,19 @@
-module.exports.getAnimals = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      animals: 'TBD...',
-    }),
-  };
+const apiUrl = 'https://apigateway.test.lifeworks.com/rescue-shelter-api';
+const request = require('request-promise');
 
-  callback(null, response);
+module.exports.getAnimals = (event, context, callback) => {
+  const cats = request(`${apiUrl}/cats`);
+  const dogs = request(`${apiUrl}/dogs`);
+  const hamsters = request(`${apiUrl}/hamsters`);
+
+  Promise.all([cats, dogs, hamsters]).then((values) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        animals: values,
+      }),
+    };
+
+    callback(null, response);
+  });
 };
