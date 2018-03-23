808c50a87245f4bd418464dd9d25a489b6171d38
Dmitry Livchak <dmitry.livchak@gmail.com>
2018-03-23 15:00:01 +0000

    Return an array of animals

--- handler.js
+++ handler.js
@@ -2,15 +2,15 @@ const apiUrl = 'https://apigateway.test.lifeworks.com/rescue-shelter-api';
 const request = require('request-promise');
 
 module.exports.getAnimals = (event, context, callback) => {
-  const cats = request(`${apiUrl}/cats`);
-  const dogs = request(`${apiUrl}/dogs`);
-  const hamsters = request(`${apiUrl}/hamsters`);
+  const cats = request(`${apiUrl}/cats`).then(value => JSON.parse(value).body);
+  const dogs = request(`${apiUrl}/dogs`).then(value => JSON.parse(value).body);
+  const hamsters = request(`${apiUrl}/hamsters`).then(value => JSON.parse(value).body);
 
   Promise.all([cats, dogs, hamsters]).then((values) => {
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        animals: values,
+        animals: [].concat(...values),
       }),
     };
 
